10c48de864df71824ed449395f9ba9abfd161f2f
Dan Dart <git@dandart.co.uk>
2025-08-14 17:05:42 +0100

    might work a bit better now

--- lib/DB/Instances/TH.hs
+++ lib/DB/Instances/TH.hs
@@ -18,74 +18,74 @@ concatMapQ ∷ (a → Q [b]) → [a] → Q [b]
 concatMapQ x = foldQ . fmap x
 
 deriveDBInstances ∷ Model → Q [Dec]
-deriveDBInstances Model { modelName, fields, extraViewFields } = do
+deriveDBInstances Model { singularType, createFields, retrieveFields, updateFields, deleteFields } = do
 
     let fieldInstances ∷ Q [Dec]
         fieldInstances = foldQ $
             fmap (\Field { upperField = upperField, typeName = typeName } -> do
-                -- let modelName' = mkName modelName
-                let fieldName = mkName $ "Create" <> modelName <> upperField
+                -- let singularType' = mkName singularType
+                let fieldName = mkName $ "Create" <> singularType <> upperField
                 -- InstanceD Nothing [] ConT typeName []
                 [d|
                     deriving via $(conT typeName) instance ToField $(conT fieldName)
                     |]
-                ) fields <>
+                ) (addDefaultFieldsForCreate createFields) <>
             fmap (\Field { upperField = upperField, typeName = typeName } -> do
-                -- let modelName' = mkName modelName
-                let fieldName = mkName $ modelName <> upperField
+                -- let singularType' = mkName singularType
+                let fieldName = mkName $ singularType <> upperField
                 -- InstanceD Nothing [] ConT typeName []
                 [d|
                     deriving via $(conT typeName) instance FromField $(conT fieldName)
                     deriving via $(conT typeName) instance ToField $(conT fieldName)
                     |]
-                ) (addDefaultFieldsForRetrieve (fields <> extraViewFields)) <>
+                ) (addDefaultFieldsForRetrieve retrieveFields) <>
             fmap (\Field { upperField = upperField, typeName = typeName } -> do
-                -- let modelName' = mkName modelName
-                let fieldName = mkName $ "Update" <> modelName <> upperField
+                -- let singularType' = mkName singularType
+                let fieldName = mkName $ "Update" <> singularType <> upperField
                 -- InstanceD Nothing [] ConT typeName []
                 [d|
                     deriving via $(conT typeName) instance FromField $(conT fieldName)
                     deriving via $(conT typeName) instance ToField $(conT fieldName)
                     |]
-                ) (addDefaultFieldsForUpdate fields) <>
+                ) (addDefaultFieldsForUpdate updateFields) <>
             fmap (\Field { upperField = upperField, typeName = typeName } -> do
-                -- let modelName' = mkName modelName
-                let fieldName = mkName $ "Delete" <> modelName <> upperField
+                -- let singularType' = mkName singularType
+                let fieldName = mkName $ "Delete" <> singularType <> upperField
                 -- InstanceD Nothing [] ConT typeName []
                 [d|
                     deriving via $(conT typeName) instance ToField $(conT fieldName)
                     |]
-                ) (addDefaultFieldsForDelete fields)
+                ) (addDefaultFieldsForDelete deleteFields)
 
     let modelInstances ∷ Q [Dec]
         modelInstances = [d|
-            instance ToRow $(conT (mkName ("Create" <> modelName)))
-            instance FromRow $(conT (mkName modelName))
-            instance ToRow $(conT (mkName ("Update" <> modelName)))
-            instance ToRow $(conT (mkName ("Delete" <> modelName)))
+            instance ToRow $(conT (mkName ("Create" <> singularType)))
+            instance FromRow $(conT (mkName singularType))
+            instance ToRow $(conT (mkName ("Update" <> singularType)))
+            instance ToRow $(conT (mkName ("Delete" <> singularType)))
             |]
 
     foldQ [fieldInstances, modelInstances]
 
 
 deriveRetrieveDBInstances ∷ Model → Q [Dec]
-deriveRetrieveDBInstances Model { modelName, fields, extraViewFields } = do
+deriveRetrieveDBInstances Model { singularType, retrieveFields } = do
 
     let fieldInstances ∷ Q [Dec]
         fieldInstances = foldQ $
             fmap (\Field { upperField = upperField, typeName = typeName } -> do
-                -- let modelName' = mkName modelName
-                let fieldName = mkName $ modelName <> upperField
+                -- let singularType' = mkName singularType
+                let fieldName = mkName $ singularType <> upperField
                 -- InstanceD Nothing [] ConT typeName []
                 [d|
                     deriving via $(conT typeName) instance ToField $(conT fieldName)
                     deriving via $(conT typeName) instance FromField $(conT fieldName)
                     |]
-                ) (addDefaultFieldsForRetrieve (fields <> extraViewFields))
+                ) (addDefaultFieldsForRetrieve retrieveFields)
 
     let modelInstances ∷ Q [Dec]
         modelInstances = [d|
-            instance FromRow $(conT (mkName modelName))
+            instance FromRow $(conT (mkName singularType))
             |]
 
     foldQ [fieldInstances, modelInstances]
